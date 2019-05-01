2d220d6a2a4b97bcaa199ade69904aa5fe4ceea8
Carlos Cueto <c.cueto.zumaya@gmail.com>
2019-05-01 12:10:59 -0500

    recupera accion

---
+++ part1.R
@@ -0,0 +1,39 @@
+
+library(quantmod)
+library(dplyr)
+library(PerformanceAnalytics)
+library(IntroCompFinR)
+library(readxl)
+
+
+# Set Workspace
+rm(list = ls())
+
+# Part 1
+periodicity = "weekly"
+
+from = "2018-01-01"
+
+to = "2018-12-31"
+
+ticker = "KIMBERA.MX"
+
+try(getSymbols(ticker,
+           from = from, to = to,
+           periodicity = periodicity,
+           src = "yahoo"))
+
+
+objList <- lapply(ticker, get)
+
+prices.zoo <- do.call(merge, objList)
+
+rm(objList)
+
+prices.df <- as.data.frame(prices.zoo) %>%
+  na.omit() %>%
+  select(contains("Adjusted"))
+
+returns.df.weekly <- as.data.frame(diff(log(as.zoo(prices.df))))
+
+return.annualized <- as.data.frame(((returns.df.weekly + 1) ** 52) - 1)
