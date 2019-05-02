02667c516c1c9baa7ea5f3ff47830f344dfdb4af
gama <alexgamaes@gmail.com>
2019-05-01 19:07:37 -0500

    Add simulation

--- part1.R
+++ part1.R
@@ -19,9 +19,9 @@ to = "2018-12-31"
 ticker = "KIMBERA.MX"
 
 try(getSymbols(ticker,
-           from = from, to = to,
-           periodicity = periodicity,
-           src = "yahoo"))
+               from = from, to = to,
+               periodicity = periodicity,
+               src = "yahoo"))
 
 
 objList <- lapply(ticker, get)
@@ -36,8 +36,6 @@ prices.df <- as.data.frame(prices.zoo) %>%
 
 returns.weekly.zoo <- diff(log(as.zoo(prices.df)))
 returns.weekly.df = as.data.frame(returns.weekly.zoo)
-
-
 
 
 # Risk Free
@@ -55,24 +53,70 @@ rm(rfrate.obj)
 
 rfrate.df <- rfrate.zoo[index(rfrate.zoo) >= as.Date("2018-01-01") & index(rfrate.zoo) <= as.Date("2018-12-01")]
 
-risk_free_rate <- sum(rfrate.df)/100/52 
-
-
+risk_free_rate <- exp(mean(log(rfrate.df / 100))) # geometric mean of annualized risk free rate
 
 # Multiperiod binomial model
 weekly_sd <- sd(returns.weekly.zoo)
 stock_price <- prices.df[nrow(prices.df), ]
 
-multiperiodBinomialFunction <- function(periods, iterations, weekly_sd_cc) {
+
+applyPeriod <- function(s, q, u, d) {
+  if(runif(1) < q) {
+    return(s * u)
+  } else {
+    return(s * d)
+  }
+}
+
+multiperiodBinomialFunction <- function(periods, iterations, stock_price, weekly_sd_cc, risk_free, strike_price) {
   u <- exp(weekly_sd_cc)
   d <- 1 / u
+  r <- (1 + risk_free)
   
-  q = ()
+  q <- (r - d) / (u - d)
   
+  S <- rep(stock_price, iterations)
+  
+  for(i in 1:periods) {
+    S <- sapply(S, applyPeriod, q = q, u = u, d = d, simplify = "array")
+  }
+  
+  s_mean <- mean(S)
+  s_sd <- sd(S)
+  
+  u52 <- u ** periods
+  d52 <- d ** periods
+  r52 <- r ** periods
+  print(u)
+  print(d)
+  print("J")
+  print(u52)
+  print(d52)
+  print(r52)
+  
+  Su <- (stock_price * u52)
+  Sd <- (stock_price * d52)
+  
+  Cu <- max(Su -  strike_price, 0)
+  Cd <- max(Sd -  strike_price, 0)
+  
+  B0 <- (u52 * Cd - d52 * Cu) / (r52 * (u52 - d52))
+  N <- (Cu - Cd) / (Su - Sd)
+  P0 <- B0 + N * stock_price
+  
+  print(Su)
+  print(Sd)
+  print(Cu)
+  print(Cd)
+  print(B0)
+  print(N)
+  print(P0)
   
 }
 
-multiperiodBinomialFunction(52, 10000, weekly_sd)
+strike_price = 40
+
+multiperiodBinomialFunction(52, 10000, stock_price, weekly_sd, risk_free_rate/52, strike_price) 
 
 # Black and Sholes model
 
