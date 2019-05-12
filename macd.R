1ee2f07b581d4bc0d870bd1fb4b6547073e0d1d9
Alex Gama <alexgamaes@gmail.com>
2019-05-12 16:37:28 -0500

    higher lows

--- macd.R
+++ macd.R
@@ -44,4 +44,47 @@ plot(lel$macd)
 lines(lel$signal, col="blue")
 lines(lel$macdOsc, col="red")
 
-polygon(cbind(c(min(index(lel)), index(lel), max(index(lel))), c(min(lel$macd), lel$macd, min(lel$macd))), col="#00CC66") 
+polygon(cbind(c(min(index(lel)), index(lel), max(index(lel))), c(min(lel$macd), lel$macd, min(lel$macd))), col="#00CC66")
+
+
+
+get_higher_lows <- function(data) {
+  days_to_acum <- 5
+  
+  differences <- diff(data)
+  sd_data <- sd(data)
+  
+  
+  past <- rep(NA, length(differences))
+  for(i in days_to_acum:length(differences)) {
+    past[i] <- mean(differences[i:(i - days_to_acum + 1)])
+  }
+  print(past)
+  
+  future <- rep(NA, length(differences))
+  for(i in (length(differences) - days_to_acum + 1):1) {
+    future[i] <- mean(differences[i:(i + days_to_acum - 1)])
+  }
+  print(future)
+
+  threshold <-  (sd_data * 0.01)
+
+  higher_lows <- c()
+  for(i in days_to_acum:(length(differences) - days_to_acum + 1)) {
+    v <- min(-past[i], future[i])
+    print(paste(i, v))
+
+    if(data[i] >= data[i + 1] && data[i + 1] <= data[i + 2] && v > threshold) {
+      print(paste(i, past[i], future[i]))
+      higher_lows <- c(higher_lows, i + 1)
+    }
+  }
+  
+  
+  return(higher_lows)
+}
+
+higher_lows <- get_higher_lows(lel$IBM.Close)
+print(higher_lows)
+
+points(higher_lows, lel$macd[higher_lows], pch=19, col ="red")
