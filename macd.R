f5e1642e567c1a8ccef1a912f245f988aabe54c7
Carlos Cueto <c.cueto.zumaya@gmail.com>
2019-05-12 14:05:57 -0500

    MACD

--- macd.R
+++ macd.R
@@ -1,6 +1,6 @@
 require(quantstrat)
 require(quantmod)
-
+library(dplyr)
 library(ggplot2)
 
 
@@ -22,18 +22,26 @@ objList <- lapply(ticker, get)
 
 prices.zoo <- do.call(merge, objList)
 
+EMA(Cl(prices.zoo), n=12)
+
+EMA(Cl(prices.zoo), n=26)
+
 macd = MACD(Cl(prices.zoo), nFast=12, nSlow=26, nSig=9, maType=EMA, percent = FALSE)
 
 lel <- merge(Cl(prices.zoo), macd)
-
 lel$macdOsc <- lel$macd - lel$signal
 
-lel$Trader_Action <- ifelse((lel$macdOsc > 0), "Buy", "Sell")
+lel = as.data.frame(lel)
 
-tail(lel)
+# Check if the 
+lel$Bullish_Bearish_Crossover = ifelse((lel$macd > lel$signal), "Bullish - Buy", "Bearish - Sell")
+
+# Upside momentum is increasing
+lel$Bullish_Bearish_Centerline_Crossover = ifelse((lel$macd > 0), "Buy", "Sell")
+
 
 plot(lel$macd)
-lines(lel$signal)
-lines(lel$macdOsc)
+lines(lel$signal, col="blue")
+lines(lel$macdOsc, col="red")
 
 polygon(cbind(c(min(index(lel)), index(lel), max(index(lel))), c(min(lel$macd), lel$macd, min(lel$macd))), col="#00CC66") 
